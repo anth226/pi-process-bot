79b820af815f0ab1063d5243ce70f3fb8022bfe2
masterial <konstantin.k.yurchenko@gmail.com>
2020-06-16 09:33:59 -0700

    Fixes issue with S3 caching.

--- src/controllers/performances.js
+++ src/controllers/performances.js
@@ -5,6 +5,31 @@ const chalk = require("chalk");
 import * as titans from "./titans";
 import * as queue from "../queue";
 
+const chalk = require("chalk");
+
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const uploadToS3 = async (key, data) => {
+  let params = {
+    Bucket: process.env.BUCKET_INTRINIO_ZAKS,
+    Key: key,
+    Body: JSON.stringify(data),
+    ContentType: "application/json",
+    ACL: "public-read",
+  };
+
+  const response = await s3.upload(params).promise();
+
+  console.log(chalk.bgYellow("s3 =>"), response);
+
+  return response;
+};
+
 export async function getPortfolios() {
   return {
     url: "https://ri-terminal.s3.amazonaws.com/portfolios.json",
