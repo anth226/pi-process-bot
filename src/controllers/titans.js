c0fa55de421baa20281af58b9a12cc8f7c104f8d
Anthony Peterson <anthony@petersonrace.com>
2020-08-07 11:09:29 -0700

    Moves helper functions to titans

--- src/controllers/titans.js
+++ src/controllers/titans.js
@@ -4,8 +4,6 @@ import * as queue from "../queue";
 
 import * as holdings from "./holdings";
 
-import { fetchBillionaireList, getNetworth } from "../forbes";
-
 import { orderBy } from "lodash";
 
 export async function getTitans({ sort = [], page = 0, size = 100, ...query }) {
@@ -331,6 +329,41 @@ const evaluateFundPerformace = async (cik) => {
     console.error(e);
   }
 };
+
+export async function fetchBillionaireList() {
+  let billionaires = [];
+
+  try {
+    let url = `https://www.forbes.com/ajax/list/data?year=2020&uri=billionaires&type=person`;
+    const result = await axios.get(url);
+    billionaires = result.data;
+  } catch (e) {
+    console.error(e);
+  }
+  return billionaires;
+}
+
+export async function getNetworth(list, uri) {
+  let value;
+
+  //   try {
+  //     let url = `https://www.forbes.com/profile/${uri}`;
+  //     const result = await axios.get(url);
+  //     const $ = await cheerio.load(result.data);
+  //     value = $("div.profile-info__item-value").text();
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+
+  //   console.log("value", value);
+
+  let billionaire = await find(list, { uri });
+
+  console.log(billionaire);
+
+  value = billionaire["realTimeWorth"];
+  return value;
+}
 
 export async function updateNetWorth(id) {
   console.log(id);
