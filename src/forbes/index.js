850fe1db02cc89b32eef81a718455fd9a08ef92d
endlessorhub <anthonyhutchi99@gmail.com>
2020-08-07 11:09:29 -0700

    Moves helper functions to titans

--- src/forbes/index.js
+++ src/forbes/index.js
@@ -2,44 +2,11 @@ import axios from "axios";
 import { find } from "lodash";
 // import cheerio from "cheerio";
 
+import { fetchBillionaireList, getNetworth } from "../controllers/titans";
+
 (async () => {
   let list = await fetchBillionaireList();
   let value = await getNetworth(list, "warren-buffett");
 
   console.log(value);
 })();
-
-export async function fetchBillionaireList() {
-  let billionaires = [];
-
-  try {
-    let url = `https://www.forbes.com/ajax/list/data?year=2020&uri=billionaires&type=person`;
-    const result = await axios.get(url);
-    billionaires = result.data;
-  } catch (e) {
-    console.error(e);
-  }
-  return billionaires;
-}
-
-export async function getNetworth(list, uri) {
-  let value;
-
-  //   try {
-  //     let url = `https://www.forbes.com/profile/${uri}`;
-  //     const result = await axios.get(url);
-  //     const $ = await cheerio.load(result.data);
-  //     value = $("div.profile-info__item-value").text();
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-
-  //   console.log("value", value);
-
-  let billionaire = await find(list, { uri });
-
-  console.log(billionaire);
-
-  value = billionaire["realTimeWorth"];
-  return value;
-}
