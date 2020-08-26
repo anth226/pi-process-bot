ab62a6cfb5b440127eef424ba08f10c051a61734
masterial <konstantin.k.yurchenko@gmail.com>
2020-08-26 12:49:26 -0700

    Implements endpoint for updating global dashboard.

---
+++ src/controllers/widgets.js
@@ -0,0 +1,28 @@
+import db from "../db";
+import * as queue from "../queue";
+
+export async function getWidgets() {
+  let result = await db(`
+    SELECT widget_instances.*, widget_data.*, widgets.*
+    FROM widget_instances
+    JOIN widget_data ON widget_data.id = widget_instances.widget_data_id 
+    JOIN widgets ON widgets.id = widget_instances.widget_id 
+  `);
+
+  return result;
+}
+
+export async function update() {
+  let widgets = await getWidgets();
+
+  for (let i = 0; i < widgets.length; i += 1) {
+    let { type, dashboard_id } = widgets[0];
+
+    console.log(widgets[0]);
+    console.log(type);
+
+    if (dashboard_id == 0) {
+      await queue.publish_UpdateGlobalDashboard(widget);
+    }
+  }
+}
