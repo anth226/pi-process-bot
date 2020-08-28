e9fc740be42ad509bb746380bbc0ac761e6ce39f
endlessorhub <anthonyhutchi99@gmail.com>
2020-08-28 14:18:58 -0700

    Adds new queue for updating widgets

--- src/controllers/widgets.js
+++ src/controllers/widgets.js
@@ -12,23 +12,149 @@ export async function getWidgets() {
   return result;
 }
 
+export async function getWidget(widgetInstanceId) {
+  let result = await db(`
+    SELECT widget_instances.*, widget_data.*, widgets.*
+    FROM widget_instances
+    JOIN widget_data ON widget_data.id = widget_instances.widget_data_id 
+    JOIN widgets ON widgets.id = widget_instances.widget_id
+    WHERE widget_instances.id = ${widgetInstanceId}
+  `);
+
+  return result;
+}
+
 export async function update() {
   let widgets = await getWidgets();
 
   for (let i = 0; i < widgets.length; i += 1) {
-    let { type, dashboard_id } = widgets[0];
-
-    console.log(widgets[0]);
-    console.log(type);
+    let widget = widgets[i];
+    let dashboard_id = widget.dashboard_id;
+    let widgetInstanceId = widget.id;
 
     if (dashboard_id == 0) {
-      await queue.publish_UpdateGlobalDashboard(widget);
+      await queue.publish_UpdateGlobalDashboard(widgetInstanceId);
     }
   }
 }
 
 export async function processWidget(widgetInstanceId) {
-  // use type to decern what function to run ex. MutualFundsTopNDiscount
-  // use input relevant input to customize ex. count = 10
-  // load result into output for widget_data
+  let result = await getWidget(widgetInstanceId);
+  let widget;
+
+  if (result) {
+    widget = result[0];
+  }
+
+  if (widget) {
+    let type = widget.type;
+    let input = widget.input;
+    let dataId = widget.widget_data_id;
+    let params = {};
+    if (input) {
+      Object.entries(input).map((item) => {
+        params[item[0]] = item[1];
+      });
+    }
+
+    /* MUTUAL FUNDS */
+    //Discount/Premium
+    if (type == "MutualFundsTopNDiscount" || type == "MutualFundsTopNPremium") {
+      let topFunds;
+      if (params.count && params.count > 0) {
+        let topNum = params.count;
+        if (type == "MutualFundsTopNDiscount") {
+          topFunds = await getMutualFundsTopNDiscountOrPremium(topNum, true);
+        } else if (type == "MutualFundsTopNPremium") {
+          topFunds = await getMutualFundsTopNDiscountOrPremium(topNum, false);
+        }
+        if (topFunds) {
+          let query = {
+            text: "UPDATE widget_data SET output = $2 WHERE id = $1",
+            values: [dataId, topFunds],
+          };
+          await db(query);
+          console.log("output updated");
+        }
+      }
+    }
+  }
+}
+
+export async function getMutualFundsTopNDiscountOrPremium(topNum, isDiscount) {
+  let eFunds = [];
+  let fFunds = [];
+  let oFunds = [];
+
+  let result = await db(`
+    SELECT *
+    FROM mutual_funds
+  `);
+
+  if (result.length > 0) {
+    for (let i in result) {
+      let fund = result[i];
+      let fundCategory = fund.json.fundCategory;
+
+      if (fund["json"]) {
+        if (fund["json"]["nav"] && fund["json"]["mktPrice"]) {
+          let difference = (
+            (fund.json.mktPrice / fund.json.nav - 1) *
+            100
+          ).toFixed(2);
+          if (fundCategory[0] == "E") {
+            eFunds.push({
+              fund: fund,
+              diff: difference,
+            });
+          } else if (fundCategory[0] == "F") {
+            fFunds.push({
+              fund: fund,
+              diff: difference,
+            });
+          } else if (fundCategory[0] == "H" || fundCategory[0] == "C") {
+            oFunds.push({
+              fund: fund,
+              diff: difference,
+            });
+          }
+        }
+      }
+    }
+  }
+  let equityFunds;
+  let fixedFunds;
+  let otherFunds;
+
+  switch (isDiscount) {
+    case true:
+      equityFunds = eFunds
+        .sort((a, b) => b.diff - a.diff)
+        .slice(Math.max(eFunds.length - topNum, 0));
+      fixedFunds = fFunds
+        .sort((a, b) => b.diff - a.diff)
+        .slice(Math.max(fFunds.length - topNum, 0));
+      otherFunds = oFunds
+        .sort((a, b) => b.diff - a.diff)
+        .slice(Math.max(oFunds.length - topNum, 0));
+    case false:
+      equityFunds = eFunds
+        .sort((a, b) => a.diff - b.diff)
+        .slice(Math.max(eFunds.length - topNum, 0));
+      fixedFunds = fFunds
+        .sort((a, b) => a.diff - b.diff)
+        .slice(Math.max(fFunds.length - topNum, 0));
+      otherFunds = oFunds
+        .sort((a, b) => a.diff - b.diff)
+        .slice(Math.max(oFunds.length - topNum, 0));
+  }
+
+  let funds = {
+    equityFunds,
+    fixedFunds,
+    otherFunds,
+  };
+
+  return funds;
+  //console.log(funds);
 }
