e75c833215ef25cedbff010eecff221f381dc5be
endlessorhub <anthonyhutchi99@gmail.com>
2020-09-14 14:15:05 -0700

    Adds mutual funds price widget

--- src/controllers/mutualfunds.js
+++ src/controllers/mutualfunds.js
@@ -7,44 +7,17 @@ import * as titans from "./titans";
 import * as queue from "../queue";
 // import * as queue from "../queue2";
 
-/*
-import axios from "axios";
+export async function getMutualFundByTicker(ticker) {
+  let result = await db(`
+        SELECT *
+        FROM mutual_funds
+        WHERE ticker = '${ticker}'
+    `);
 
-import db from "../db";
-
-const chalk = require("chalk");
-
-const AWS = require("aws-sdk");
-
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
-
-
-
-
-
-
-
-const uploadToS3 = async (key, data) => {
-  let params = {
-    Bucket: process.env.BUCKET_INTRINIO_ZAKS,
-    Key: key,
-    Body: JSON.stringify(data),
-    ContentType: "application/json",
-    ACL: "public-read",
-  };
-
-  const response = await s3.upload(params).promise();
-
-  console.log(chalk.bgYellow("s3 =>"), response);
-
-  return response;
-};
-
-*/
+  if (result && result.length > 0) {
+    return result[0];
+  }
+}
 
 export async function getJsonMutualFunds() {
   let funds = [];
