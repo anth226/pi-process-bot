d961f74ec1c4d2dabb151144e3d3fdfdf3fad802
Anthony Peterson <anthony@petersonrace.com>
2020-12-02 00:19:20 -0800

    Fixes date

--- src/controllers/earnings.js
+++ src/controllers/earnings.js
@@ -22,7 +22,7 @@ const indexAPI = new intrinioSDK.IndexApi();
 
 export async function getDailyEarnings() {
   try {
-    let today = new Date();
+    let today = new Date().toISOString().slice(0, 10);
 
     let url = `${process.env.INTRINIO_BASE_PATH}/zacks/eps_surprises?start_date=${today}&end_date=${today}&api_key=${process.env.INTRINIO_API_KEY}`;
 
