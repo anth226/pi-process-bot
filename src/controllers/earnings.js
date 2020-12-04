3c80cfae8c5770bd7085f631b4c50c2970664bf1
Anthony Peterson <anthony@petersonrace.com>
2020-12-03 16:43:23 -0800

    rounds percentage

--- src/controllers/earnings.js
+++ src/controllers/earnings.js
@@ -182,6 +182,7 @@ export async function updateEarnings() {
     if (result.length > 0) {
       if (!eps_percent_diff && eps_actual && result[0].eps_estimate) {
         suprise_percentage = (eps_actual / result[0].eps_estimate - 1) * 100;
+        suprise_percentage.toFixed(2);
       } else {
         suprise_percentage = eps_percent_diff;
       }
